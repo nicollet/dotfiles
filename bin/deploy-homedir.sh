9e5142b28bf3b413ddd80676a3fb3c3b2433d1a0
Xavier Nicollet <xnicollet@stackexchange.com>
2016-12-14 22:03:37 -0500

    Added script to deploy homedir

---
+++ bin/deploy-homedir.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+SSH_OPT='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+SRC="$HOME/stack/git/Github/nicollet/dotfiles/.git"
+
+curl -s bosun/api/host | \
+  jq '.[]|select( (.OS.Version|strings|test("CentOS")) and (.Name|test("ny-|co-")) )|.Name' | \
+  head -1 | \
+  while read i ; do
+    host=${i//\"}
+    rsync -ae "ssh $SSH_OPT" --delete $SRC/ $host:/home/xnicollet/.git/
+    ssh -nA $SSH_OPT $host "git reset --hard origin/master" # ;git submodule init; git submodule update"
+  done
