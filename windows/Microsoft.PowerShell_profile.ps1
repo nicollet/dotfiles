82e2d14134ae98addb1ad4a6a7e95cdd7f27013f
Xavier Nicollet <xnicollet@stackexchange.com>
2017-02-21 09:35:29 -0500

    Add PreviousHistory/NextHistory that can be missing.

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -11,4 +11,6 @@ if ($host.Name -eq 'ConsoleHost')
 {
 	Import-Module PSReadline
 	Set-PSReadlineOption -EditMode Emacs
+	Set-PSReadlineKeyHandler -Key Ctrl+p -Function PreviousHistory
+	Set-PSReadlineKeyHandler -Key Ctrl+n -Function NextHistory
 }
